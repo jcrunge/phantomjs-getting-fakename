f3fceb17df7f65abdf4d44026e4d88dce529c342
jcrunge <juan.crunge@gmail.com>
2016-05-19 18:49:20 -0500

    push

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # phantomjs-getting-fakename
+
+If you want to get a alias name or fake name, this is for you. I created it with Phantomjs because It fast and you can access and manipulate webpages.
+
+[PhantomJS](http://phantomjs.org/) inquiry for more information.
---
+++ fakename.js
@@ -0,0 +1,31 @@
+var page = require('webpage').create();
+
+var url = 'http://es.fakenamegenerator.com/gen-random-sp-sp.php'; //pagina que se esta abriendo
+
+page.open(url, function (status){
+  if (status !== "success") {
+    console.log("No se puede Acceder al sitio");
+    phantom.exit();
+  } else {
+
+  var pnombre = page.evaluate(function(sol){
+    var nombre = ".address > h3:nth-child(1)"; //obtiene información de selector unico
+    return document.querySelector(nombre).innerText.split(" ", 1);
+  }, 'pnombre');
+
+  var appellido = page.evaluate(function(select){
+    var apellido = ".address > h3:nth-child(1)";
+    var str = document.querySelector(apellido).innerText.split(" ", 1);
+    return document.querySelector(apellido).innerText.split(str, 2);
+  }, 'appellido');
+
+  var posiblecorreo = page.evaluate(function(select){
+    var correo = "dl.dl-horizontal:nth-child(11) > dd:nth-child(2)";
+    return document.querySelector(correo).innerText;
+  }, 'posiblecorreo');
+
+  console.log("Nombre: " + pnombre, "\nApellidos: " + appellido, "\nCorreo: " + posiblecorreo + "@gmail.com" ); //muestra en consola
+
+  phantom.exit(); //siempre se debera escribir salir del navegador de lo contrario nunca se cerrara
+  }
+});
